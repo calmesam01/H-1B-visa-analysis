4338fa911b214c78c40df9150b0759d4e16e50ec
calmesam01 <sanyamsareen01@gmail.com>
2017-12-30 00:02:24 +0530

    employer and city statistics

--- emp-site.R
+++ emp-site.R
@@ -5,17 +5,21 @@ visa = read.csv("h1b.csv",stringsAsFactors = FALSE)
 head(visa)
 
 #Next step is to find the city where highest paid employee lives.
+max_h1b = which.max(visa$PREVAILING_WAGE[1:100000])
 max_site_h1b = visa$WORKSITE[max_h1b]
 max_site_h1b
 
 #Let us find the city where lowest paid employee lives.
+min_h1b = which.min(visa$PREVAILING_WAGE[1:100000])
 min_site_h1b = visa$WORKSITE[min_h1b]
 min_site_h1b
 
 #The employer who pays the maximum wages.
+max_h1b = which.max(visa$PREVAILING_WAGE[1:100000])
 max_emp_h1b = visa$EMPLOYER_NAME[max_h1b]
 max_emp_h1b
 
 #The employer who overexploits his workers the most.
+min_h1b = which.min(visa$PREVAILING_WAGE[1:100000])
 min_emp_h1b = visa$EMPLOYER_NAME[min_h1b]
 min_emp_h1b
